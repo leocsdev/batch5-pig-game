b2bdfe839aeca8b00c5115f909a4731d654f0132
Leo <leocsdev@gmail.com>
2021-01-09 12:05:12 +0800

    Linked DOM elements to js.

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Initialize necessary variables
+
+// global score
+const globalScore = 100;
+
+// buttons 
+const btnNew = document.querySelector(".btn-new");
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold");
+
+// player names
+const p0Name = document.getElementById("name-0");
+const p1Name = document.getElementById("name-1");
+
+// player dice roll scores
+const p0RollScore = document.getElementById("score-0");
+const p1RollScore = document.getElementById("score-1");
+
+// player current scores (when on HOLD)
+const p0CurrentScore = document.getElementById("current-0");
+const p1CurrentScore = document.getElementById("current-1");
+
+const player0Toggle = document.querySelector(".player-0-panel");
+const player1Toggle = document.querySelector(".player-1-panel");
